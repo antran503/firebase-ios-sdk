ec2a2282f746e8d398172340af84d803b98f8dec
Maksym Malyhin <mmaksym@google.com>
2019-08-09 16:02:06 -0400

    sudo

--- kokoro/scripts/core_lib_lint.sh
+++ kokoro/scripts/core_lib_lint.sh
@@ -20,7 +20,7 @@ ROOT_DIR=$KOKORO_ARTIFACTS_DIR/github/firebase-ios-sdk
 # DEVELOPER_DIR=/Applications/Xcode_10.1.app
 sudo xcode-select -s /Applications/Xcode_10.1.app
 
-xcode-select -p
+sudo xcode-select -p
 
 sudo
 
