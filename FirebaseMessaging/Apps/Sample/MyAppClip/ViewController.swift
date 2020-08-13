0663f87c639db2c1dfd542cda25a748d3f37e0b1
Chen Liang <chliang@google.com>
2020-08-13 16:28:27 -0700

    add app clip

---
+++ FirebaseMessaging/Apps/Sample/MyAppClip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppClip
+//
+//  Created by Chen Liang on 8/11/20.
+//  Copyright © 2020 Google, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
