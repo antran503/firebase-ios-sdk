8f52a40a7b865141811f3cc96a19cfc97ab7b338
Chris Tibbs <eggyeggbean@google.com>
2021-02-09 16:38:07 -0500

    Working delegate bridge situation

--- FirebaseInAppMessaging/Swift/Source/DelegateBridge.swift
+++ FirebaseInAppMessaging/Swift/Source/DelegateBridge.swift
@@ -4,21 +4,30 @@ import SwiftUI
 // Handle delegate for FIAM actions.
 struct InAppMessagingDisplayModifier<DisplayMessage: View>: ViewModifier {
   var closure: (InAppMessagingDisplayMessage, InAppMessagingDisplayDelegate) -> DisplayMessage
-  
+
   @ObservedObject var delegateBridge: DelegateBridge = DelegateBridge()
+
+  init(closure: @escaping (InAppMessagingDisplayMessage, InAppMessagingDisplayDelegate)
+    -> DisplayMessage) {
+    self.closure = closure
+  }
 
   func body(content: Content) -> some View {
     let inAppMessage = delegateBridge.inAppMessage
     return content
-      .overlay(inAppMessage == nil ? AnyView(EmptyView()) : AnyView(closure(inAppMessage!.0, inAppMessage!.1)))
+      .overlay(inAppMessage == nil ? AnyView(EmptyView()) :
+        AnyView(closure(inAppMessage!.0, inAppMessage!.1)))
   }
 }
 
-class DelegateBridge: InAppMessagingDisplay, ObservableObject {
-  @Published var inAppMessage: (InAppMessagingDisplayMessage, InAppMessagingDisplayDelegate)?  = nil
+class DelegateBridge: NSObject, InAppMessagingDisplay, InAppMessagingDisplayDelegate,
+  ObservableObject {
+  @Published var inAppMessage: (InAppMessagingDisplayMessage, InAppMessagingDisplayDelegate)? = nil
 
-  init() {
+  override init() {
+    super.init()
     InAppMessaging.inAppMessaging().messageDisplayComponent = self
+    InAppMessaging.inAppMessaging().delegate = self
   }
 
   func displayMessage(_ messageForDisplay: InAppMessagingDisplayMessage,
@@ -27,11 +36,23 @@ class DelegateBridge: InAppMessagingDisplay, ObservableObject {
       self.inAppMessage = (messageForDisplay, displayDelegate)
     }
   }
+
+  func messageClicked(_ inAppMessage: InAppMessagingDisplayMessage,
+                      with action: InAppMessagingAction) {
+    self.inAppMessage = nil
+  }
+
+  func messageDismissed(_ inAppMessage: InAppMessagingDisplayMessage,
+                        dismissType: FIRInAppMessagingDismissType) {
+    self.inAppMessage = nil
+  }
 }
 
+// View modifier that takes a closure for handling in-app message display.
 public extension View {
   func onDisplayInAppMessage<T: View>(closure: @escaping (InAppMessagingDisplayMessage,
-                                                          InAppMessagingDisplayDelegate) -> T) -> some View {
+                                                          InAppMessagingDisplayDelegate) -> T)
+    -> some View {
     modifier(InAppMessagingDisplayModifier(closure: closure))
   }
 }
